9ee430ed4c86db400d9c3339c0ffa00f6b086502
codeniu <codeniu@163.com>
2023-06-21 10:28:37 +0000

    update script for juejin

--- .github/scripts/update_posts.rb
+++ .github/scripts/update_posts.rb
@@ -3,30 +3,18 @@ require 'nokogiri'
 require 'octokit'
 
 # Scrape blog posts from the website
-url = "https://www.bengreenberg.dev/blog/"
+url = "https://juejin.cn/user/1732486058223176/posts"
 response = HTTParty.get(url)
+# puts response.body
 parsed_page = Nokogiri::HTML(response.body)
-posts = parsed_page.css('.flex.flex-col.rounded-lg.shadow-lg.overflow-hidden')
-
+posts = parsed_page.css('.content-main .title')
+# puts posts
 
 # Generate the updated blog posts list (top 5)
 posts_list = ["\n### Recent Blog Posts\n\n"]
 posts.first(5).each do |post|
-  title = post.css('p.text-xl.font-semibold.text-gray-900').text.strip
-  link = "https://www.bengreenberg.dev#{post.at_css('a')[:href]}"
+  title = post.text
+  link = "https://juejin.cn/post/#{post[:href]}"
   posts_list << "* [#{title}](#{link})"
 end
 puts posts_list
-
-# Update the README.md file
-client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-repo = ENV['GITHUB_REPOSITORY']
-readme = client.readme(repo)
-readme_content = Base64.decode64(readme[:content]).force_encoding('UTF-8')
-
-# Replace the existing blog posts section
-posts_regex = /### Recent Blog Posts\n\n[\s\S]*?(?=<\/td>)/m
-updated_content = readme_content.sub(posts_regex, "#{posts_list.join("\n")}\n")
-puts updated_content
-
-client.update_contents(repo, 'README.md', 'Update recent blog posts', readme[:sha], updated_content)
